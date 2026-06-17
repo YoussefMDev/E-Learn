b70fe391e586eb2ab3cc541819ac853f7e2042cf
yousef <mobasheryousef4@gmail.com>
2026-06-17 17:07:31 +0300

    Fix admin controller missing functions

--- config/database.js
+++ config/database.js
@@ -1,13 +1,31 @@
 const mongoose = require('mongoose');
 
+
+let isConnected = false;
+
 const connectDB = async () => {
+    
+    if (isConnected) {
+        console.log('=> استخدام اتصال قاعدة البيانات النشط حالياً');
+        return;
+    }
+
+    
+    if (!process.env.MONGO_URI) {
+        console.error('=> خطأ: لم يتم العثور على متغير البيئة MONGO_URI!');
+        return;
+    }
+
+    console.log('=> إنشاء اتصال جديد بقاعدة البيانات...');
     try {
-        // تمرير الرابط فقط بدون أي خيارات إضافية
-        const conn = await mongoose.connect(process.env.MONGO_URI);
-        console.log(`MongoDB Connected: ${conn.connection.host}`);
+        const db = await mongoose.connect(process.env.MONGO_URI);
+        
+        // حفظ حالة الاتصال (1 تعني متصل بنجاح)
+        isConnected = db.connections[0].readyState;
+        console.log(`MongoDB Connected: ${db.connection.host}`);
     } catch (error) {
         console.error(`Database Connection Error: ${error.message}`);
-        process.exit(1); 
+        throw error;
     }
 };
 
